89a46cc24ea322455d355d263fa0c397bf3489e2
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2026-01-27 17:47:05 +0700

    fix: port from env

--- bg-remove-service/src/bg_remove_service/main.py
+++ bg-remove-service/src/bg_remove_service/main.py
@@ -75,4 +75,6 @@ async def remove_background(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    import os
+    port = int(os.getenv("PORT", 8001))
+    uvicorn.run(app, host="0.0.0.0", port=port)
